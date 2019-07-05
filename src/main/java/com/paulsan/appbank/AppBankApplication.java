d192c8a8826c910fad40529decfde1faefefb1f2
Paul Sanchez Pardo <psanchez@aj.gob.bo>
2019-07-05 16:14:39 -0400

    set config sÂpring application

---
+++ src/main/java/com/paulsan/appbank/AppBankApplication.java
@@ -0,0 +1,13 @@
+package com.paulsan.appbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBankApplication.class, args);
+	}
+
+}
